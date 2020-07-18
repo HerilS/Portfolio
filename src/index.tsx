9fa93ed642f5a11306c8c9c480d5b437a5c93bec
HerilS <herilsaha06@gmail.com>
2020-07-18 13:31:16 -0600

    Converted CRA to Gatsby

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
-import PanelOne from './components/PanelOne';
-import PanelTwo from './components/PanelTwo';
-import PanelThree from './components/PanelThree';
-import PanelFour from './components/PanelFour';
-import PanelFive from './components/PanelFive';
-import PanelSix from './components/PanelSix';
-import PanelSeven from './components/PanelSeven';
-import * as serviceWorker from './serviceWorker';
+import './index.css';
+import App from './pages/index';
 
 ReactDOM.render(
   <React.StrictMode>
-    <PanelOne />
-    <PanelTwo />
-    <PanelThree />
-    <PanelFour />
-    <PanelFive />
-    <PanelSix />
-    <PanelSeven />
+    <App />
   </React.StrictMode>,
   document.getElementById('root'),
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import PanelOne from '../components/PanelOne';
+import PanelTwo from '../components/PanelTwo';
+import PanelThree from '../components/PanelThree';
+import PanelFour from '../components/PanelFour';
+import PanelFive from '../components/PanelFive';
+import PanelSix from '../components/PanelSix';
+import PanelSeven from '../components/PanelSeven';
+
+export default () => (
+  <React.Fragment>
+    <PanelOne />
+    <PanelTwo />
+    <PanelThree />
+    <PanelFour />
+    <PanelFive />
+    <PanelSix />
+    <PanelSeven />
+  </React.Fragment>
+);
